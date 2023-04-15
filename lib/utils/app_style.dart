01290c14afe5bc5008a8a697fe55134faf64c6d9
Matt <matheusc039@hotmail.com>
2023-04-14 22:58:32 -0300

    feat: class with all colors used within the app

---
+++ lib/utils/app_style.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  // Colors
+  static Color primaryColor = const Color(0xFF687daf);
+  static Color greyColor = Colors.grey.shade500;
+  static Color textColor = const Color(0xFF3b3b3b);
+  static Color bgColor = const Color(0xFFeeedf2);
+  static Color orangeColor = const Color(0xFFF37B67);
+
+  // TextStyle
+  static TextStyle textStyle = TextStyle(
+    fontSize: 16,
+    color: textColor,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle headLineStyle = TextStyle(
+    fontSize: 26,
+    color: textColor,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle headLineStyle2 = TextStyle(
+    fontSize: 21,
+    color: textColor,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle headLineStyle3 = TextStyle(
+    fontSize: 17,
+    color: greyColor,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle headLineStyle4 = TextStyle(
+    fontSize: 14,
+    color: greyColor,
+    fontWeight: FontWeight.w500,
+  );
+}
